9269526448371b7060d9fb8dd1297dd8f625901d
Sebastian Deppisch <git@sebastiandeppisch.de>
2018-04-10 00:35:02 +0200

    add begin of Lagrange class

---
+++ src/Math/Lagrange.php
@@ -0,0 +1,16 @@
+<?php
+namespace Math;
+class Lagrange{
+	private $func;
+	public function __construct(MFunction $func){
+		$this->func = $func;
+	}
+
+	public function getX($n){
+		return $n;
+	}
+
+	public function getL($i){
+		
+	} 	
+}
