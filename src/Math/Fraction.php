0bc7b12cabdf7a48e28664b2aa9ce95d61e5bbba
Sebastian Deppisch <git@sebastiandeppisch.de>
2018-04-10 00:34:53 +0200

    add fraction class

---
+++ src/Math/Fraction.php
@@ -0,0 +1,11 @@
+<?php
+class Fraction implements Evaluatable{
+	private $denominator;
+
+	private $numerator;
+	public function __construct(Polynomial $denominator, Polynomial $numerator){
+		$this->denominator=$denominator;
+		$this->numerator=$numerator;
+	}
+
+}
