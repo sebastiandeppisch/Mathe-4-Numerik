081f1c0d02da102b9c15a8ceda7c3d0866e39ac9
Sebastian Deppisch <git@sebastiandeppisch.de>
2018-04-09 23:07:40 +0200

    polynomial class working

--- src/Math/Polynomial.php
+++ src/Math/Polynomial.php
@@ -1,6 +1,6 @@
 <?php
 namespace Math;
-class Polynomial{
+class Polynomial implements Evaluatable{
     private $variable;
 
     private $summands=array();
@@ -9,37 +9,140 @@ class Polynomial{
         $this->variable=$variable;
     }
 
+    //internal 
 
-    public static function getFromString($string){
-
-    }
-
-    public function getDegree(){
-
-    }
-
-    public function mul($rhs){
-        if(is_numeric($rhs)){
-            $rhs = self::getFromNumber($rhs);
+    public function addSummand(PolynomialSummand $newSummand):Polynomial{
+        foreach($this->summands as &$summand){
+            if($summand->getExponentiation() === $newSummand->getExponentiation()){
+                $summand->add($newSummand);
+                return $this;
+            }
         }
+        $this->summands[]=$newSummand;
+        return $this;
     }
 
-    public function mulPolynomial(Polynomial $polynomial){
-
+    public function sort(){
+        usort($this->summands, function($a, $b){
+            $a=$a->getExponentiation();
+            $b=$b->getExponentiation();
+            if ($a == $b) {
+                return 0;
+            }
+            return ($a < $b) ? -1 : 1;
+        });
     }
 
-    public function add($rhs){
-        if(is_numeric($rhs)){
-            $rhs = self::getFromNumber($rhs);
+    public function clear():Polynomial{
+        $this->summands=[];
+        return $this;
+    }
+
+    public function getSummands(){
+        return $this->summands;
+    }
+
+    //string handling
+    private function getRegex(){
+        return sprintf('([+-]?(?:(?:\d+%s\^\d+)|(?:%s\^\d+)|(?:\d+%s)|(?:\d+)|(?:%s)))', $this->variable, $this->variable, $this->variable, $this->variable);
+    }
+
+    public function isParsable(string $formula): bool{
+        $test = preg_match_all($this->getRegex(), $formula, $matches, PREG_SET_ORDER, 0);
+        foreach($matches as $match){
+            $formula = str_replace($match[0], "", $formula);
         }
+        return strlen($formula) == 0;
     }
 
-    public function addPolynomial(Polynomial $polynomial){
+    public function addString(string $formula):Polynomial{ //TODO ugly code 
+        preg_match_all($this->getRegex(), $formula, $matches, PREG_SET_ORDER, 0);
+        foreach($matches as $match){
+            $match = $match[0];
+            $match = explode("x", $match);
+            $number = $match[0];
+            $exponentiation = 0;
+            if($number==="" || $number==="+"){
+                $number=1;
+            }
+            if($number==="-"){
+                $number=-1;
+            }
+            if(count($match) > 1 && strlen($match[1]) > 1){
+                $exponentiation = str_replace("^", "", $match[1]);
+            }else{
+                if(count($match) == 2){
+                    $exponentiation = 1;
+                }
+            }
+            $number = (int) $number;
+            $this->addSummand(new \Math\PolynomialSummand($number, $exponentiation, $this->variable));
+        }
+        return $this;
+    }
+
+    public function toString():string{ //TODO ugly code 
+        $this->sort();
+        $result="";
+        foreach($this->summands as $key => &$summand){
+            if($key !== 0){
+                $result.=(($summand->getNumber()< 0 )?"":"+");
+            }
+            if(! (abs($summand->getNumber()) == 1 && $summand->getExponentiation() !== 0)){
+                $result.=$summand->getNumber();
+            }else{
+                if($summand->getNumber() == -1){
+                    $result.="-";
+                }
+            }
+            if($summand->getExponentiation() !== 0){
+                if($summand->getExponentiation() ==1 ){
+                    $result.=$summand->getVariable();
+                }else{
+                    $result.=$summand->getVariable()."^".$summand->getExponentiation();
+                }
+                
+            }
+        }
+        if($result == ""){
+            return "0";
+        }
+        return $result;
+    }
+
+    //multiplication
+
+    public function mul(Polynomial $rhs):Polynomial{
+        $p = new Polynomial($this->variable);
+        foreach($this->summands as $a){
+            foreach($rhs->getSummands() as $b){
+                $a= $a->copy();
+                $p->addSummand($a->mul($b));
+            }
+        }
+        return $p;
+    }
+
+    //addition
+
+    public function add(Polynomial $rhs):Polynomial{
+        foreach($rhs->getSummands() as $summand){
+            $this->addSummand($summand);
+        }
+        return $this;
+    }
+
+    public function addNumber(int $number):Polynomial{
 
     }
 
-    private function handleInvariant(){
 
+    // other stuff
+    public function evaluate(int $value):int {
+        $result = 0;
+        foreach($this->summands as $summand){
+            $result+=$summand->evaluate($value);
+        }
+        return $result;
     }
-    
 }
---
+++ tests/Math/PolynomialTest.php
@@ -0,0 +1,95 @@
+<?php 
+use PHPUnit\FrameWork\TestCase;
+
+use \Math\Polynomial;
+use \Math\PolynomialSummand;
+
+class PolynomialTest extends Testcase{
+    public function testToString(){
+        $p = new Polynomial();
+        $p->addSummand(new PolynomialSummand(3, 2));
+        $this->assertEquals("3x^2", $p->toString());
+
+        $p->addSummand(new PolynomialSummand(5, 4));
+        $this->assertEquals("3x^2+5x^4", $p->toString());
+
+        $p->addSummand(new PolynomialSummand(-7, 3));
+        $this->assertEquals("3x^2-7x^3+5x^4", $p->toString());
+    }
+
+    public function testIsParsable(){
+        $p = new Polynomial();
+
+        $this->assertTrue($p->isParsable("4+10x^2+x^3"));
+        $this->assertTrue($p->isParsable("123"));
+        $this->assertTrue($p->isParsable("x"));
+        $this->assertTrue($p->isParsable("x^2"));
+        $this->assertTrue($p->isParsable("-4"));
+        $this->assertFalse($p->isParsable("y"));
+        $this->assertFalse($p->isParsable("4+10y^2+x^3"));
+    }
+
+    public function testaddString(){
+        $p = new Polynomial();
+
+        $this->assertEquals("4+10x^2+x^3", $p->addString("4+10x^2+x^3")->toString());
+        $p->clear();
+        $this->assertEquals("4", $p->addString("4")->toString());
+        $p->clear();
+        $this->assertEquals("-x", $p->addString("-x")->toString());
+        $p->clear();
+        $this->assertEquals("x^42", $p->addString("x^42")->toString());
+        $p->clear();
+        $this->assertEquals("0", $p->addString("")->toString());
+        $p->clear();
+        $this->assertEquals("0", $p->addString("0")->toString());
+        $p->clear();
+    }
+ 
+    public function testAdd(){
+        $a = new Polynomial();
+        $b = new Polynomial();
+
+        $a->addString("10x^2");
+        $b->addString("20x^2");
+        $a->add($b);
+
+        $this->assertEquals("30x^2", $a->toString());
+
+        $a->clear()->addString("42")->add($b);
+        $this->assertEquals("42+20x^2", $a->toString());
+    }
+
+    public function testEvaluate(){
+        $a = new Polynomial();
+        
+        $this->assertEquals(0, $a->evaluate(42));
+
+        $a->addString("42");
+        $this->assertEquals(42, $a->evaluate(42));
+
+        $a->addString("x");
+        $this->assertEquals(84, $a->evaluate(42));
+    }
+
+    public function testAddZero(){
+        $a = new PolynomialSummand(0);
+        $b = new PolynomialSummand(0);
+
+        $p = new Polynomial();
+        $p->addSummand($a)->addSummand($b);
+        $this->assertEquals("0", $p->toString());
+    }
+    
+    public function testMul(){
+        $a = new Polynomial();
+        $b = new Polynomial();
+
+        $a->addString("10x^2");
+        $b->addString("20x^2");
+        $c = $a->mul($b);
+        $this->assertEquals("200x^4", $c->toString());
+        $this->assertEquals("10x^2", $a->toString());
+        $this->assertEquals("20x^2", $b->toString());
+    }
+}
