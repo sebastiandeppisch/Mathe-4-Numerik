2e2b7e2377b90b928461f70db9485236084006dc
Sebastian Deppisch <git@sebastiandeppisch.de>
2018-04-13 00:32:29 +0200

    adding product class

---
+++ src/Math/Product.php
@@ -0,0 +1,11 @@
+<?php
+class Product implements Evaluatable{
+	private $factors;
+
+	private $numerator;
+	public function __construct(Polynomial $denominator, Polynomial $numerator){
+		$this->denominator=$denominator;
+		$this->numerator=$numerator;
+	}
+
+}
