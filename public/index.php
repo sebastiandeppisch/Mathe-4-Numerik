6d5df225ce2fa844a975fd5f13e6ab1eac23e483
Sebastian Deppisch <git@sebastiandeppisch.de>
2018-04-15 12:57:36 +0200

    routing stuff etc

--- public/index.php
+++ public/index.php
@@ -9,10 +9,20 @@
 	$uri = $request->server()->get('REQUEST_URI');
 	$baseURI =  dirname($_SERVER["SCRIPT_NAME"]);
 
+	$klein->respond('GET', $baseURI.'/', function (){
+		$controller = new \Controller\MainController(null);
+		
+		$loader = new Twig_Loader_Filesystem("../resources/views/");
+		$twig = new Twig_Environment($loader);
+		$template = $twig->load('page.twig');
+		return $template->render([
+			"menu" => $controller->getMenuHTML()
+		]);
+	});
+
 	foreach(\Controller\MainController::getControllerNames() as $name){
 		$klein->respond('GET', $baseURI.'/'.$name, function () use ($name){
 			$controller = new \Controller\MainController($name);
-			
 			$loader = new Twig_Loader_Filesystem("../resources/views/");
 			$twig = new Twig_Environment($loader);
 			$template = $twig->load('page.twig');
