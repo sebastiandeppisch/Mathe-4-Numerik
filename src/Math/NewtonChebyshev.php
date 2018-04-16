1ce4f03a6438c2d966193b15b8aeab2155f7198c
Sebastian Deppisch <git@sebastiandeppisch.de>
2018-04-16 21:40:37 +0200

    bug fix

--- src/Math/NewtonChebyshev.php
+++ src/Math/NewtonChebyshev.php
@@ -1,8 +1,8 @@
 <?php
 namespace Math;
-class NewtonChebyshev extends Lagrange{
+class NewtonChebyshev extends Newton{
 	public function getX($n):float{
-		return (($this->b-$this->a)/2)*cos(((2*$n+1)*pi())/(($n+1)*2))+($this->b+$this->a)/2;
+		return ((($this->b-$this->a)/2)*cos((pi()/2)*((2*$n+1))/(($n+1))))+(($this->b+$this->a)/2);
 	}
 
 }
