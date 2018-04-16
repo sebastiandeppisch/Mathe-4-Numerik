d0d942c1c2c612ac8bb055ca950167e49ae21776
Sebastian Deppisch <git@sebastiandeppisch.de>
2018-04-16 17:38:48 +0200

    add arrays for rationals and conditiont

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -27,24 +27,76 @@ abstract class Controller{
 				case "function":
 					$data = new \Math\MFunction($parameter);
 				break;
+				case "array-rational":
+					$parameters = explode(",", $parameter);
+					$data=[];
+					foreach($parameters as $rational){
+						$data[] = \Math\RationalNumber::fromString($rational);
+					}
+				break;
 			}
 			$this->data[$field["name"]]=$data;
 		}
 	}
 
+	public function getConditionsHTML(){
+		$html="<script>";
+
+		$changeableFields=array();
+		$parameters=array();
+		foreach(static::$inputFields as $field){
+			if(isset($field["show"])){
+				$p = new \stdClass();
+				$p->field=$field["name"];
+				$p->show=[];
+				foreach($field["show"] as $condition){
+					$changeableFields[]=$condition["name"];
+					$s = new \stdClass();
+					$s->name=$condition["name"];
+					$s->value=$condition["value"];
+					$p->show[]=$s;
+				}
+				$parameters[]=$p;
+				
+			}
+		}
+		$changeableFields=array_unique($changeableFields);
+		foreach($changeableFields as $field){
+			$html.='$("#'.$field.'").change(function(){toggleFields();});';
+		}
+		$html.="var parameters=".json_encode($parameters).";";
+		$html.="</script>";
+		return $html;
+	}
+
 	public function getInputHTML(){
 		$html='<form>';
 		foreach(static::$inputFields as $field){
-			$input="";
-			if(isset($this->parameters[$field["name"]])){
-				$input=' value="'.$this->parameters[$field["name"]].'"';
+			if($field["type"]=="select"){
+				$html.='<div class="form-group"><label for="'.$field["name"].'">'.$field["description"].'</label>';
+				$html.='<select class="form-control" id="'.$field["name"].'" name="'.$field["name"].'">';
+				foreach($field["values"] as $key => $value){
+					if(isset($this->parameters[$field["name"]]) && $this->parameters[$field["name"]]==$key){
+						$html.='<option value="'.$key.'" selected>'.$value.'</option>';
+					}else{
+						$html.='<option value="'.$key.'">'.$value.'</option>';
+					}
+				}
+				$html.='</select></div>';
+			}else{
+				$input="";
+				if(isset($this->parameters[$field["name"]])){
+					$input=' value="'.$this->parameters[$field["name"]].'"';
+				}
+				$html.='<div class="form-group"><label for="'.$field["name"].'">'.$field["description"].'</label>';
+				$html.='<input type="text" class="form-control" id="'.$field["name"].'" name="'.$field["name"].'"'.$input.'>';
+				$html.='</div>';
 			}
-			$html.='<div class="form-group"><label for="'.$field["name"].'">'.$field["description"].'</label>';
-			$html.='<input type="text" class="form-control" id="'.$field["name"].'" name="'.$field["name"].'"'.$input.'>';
-			$html.='</div>';
+			
 		}		
 		$html.='<button class="btn btn-primary btn-block">Berechnen</button>';
 		$html.='</form>';
+		$html.=$this->getConditionsHTML();
 		return $html;
 	}
 
@@ -55,4 +107,8 @@ abstract class Controller{
 	}
 
 	abstract function getOutputHTML();
+
+	public function getChart(){
+		return NULL;
+	}
 }
