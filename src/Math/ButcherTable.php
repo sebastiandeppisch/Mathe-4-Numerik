234c37111595c6b053498baf82ab2b40381bcbb8
Sebastian Deppisch <sebastian@Sebastians-MacBook-Pro.local>
2018-05-15 23:18:06 +0200

    add butcher table

---
+++ src/Math/ButcherTable.php
@@ -0,0 +1,106 @@
+<?php
+namespace Math;
+
+class ButcherTable{
+	private $n;
+
+	private $alphas;
+	private $betas;
+	private $gammas;
+	public function __construct(int $n){
+		$this->n=$n;
+		for($i=0;$i<$n;$i++){
+			$this->alphas=[];
+		}
+	}
+
+	public static function fromArray(array $array):ButcherTable{
+		$n = count($array)-1; // the array has the bettas as the last line => +1 
+		$b = new ButcherTable($n);
+		for($i=0;$i<=$n;$i++){
+			for($j=0;$j<=$n;$j++){
+				if($array[$i][$j] !== null){
+					$value = RationalNumber::fromString($array[$i][$j]);
+				}else{
+					$value= new RationalNumber(0);
+				}
+				if($j==0){ // first col
+					if($i!=$n){
+						$b->gammas[$i]=$value;
+					}	
+				}elseif($i==$n){ //last row 
+					$b->betas[$j-1]=$value;
+				}else{
+					$b->alphas[$i][$j-1]=$value;
+				}
+			}
+		}
+		return $b;
+	}
+
+	public function getAlpha($i, $j):Number{
+		return $this->alphas[$i-1][$j-1];
+	}
+
+	public function getBeta($i):Number{
+		return $this->betas[$i-1];
+	}
+
+	public function getGamma($i):Number{
+		return $this->gammas[$i-1];
+	}
+
+	public function getConistency():int {
+		$consistency = 0;
+
+		$sum=new RationalNumber(0);
+		for($i=1;$i<=$this->n;$i++){
+			$sum = $sum->add($this->getBeta($i)->copy());
+		}
+		if(new RationalNumber(1) == $sum){
+			$consistency = 1;
+		}
+
+		$sum = new RationalNumber(0);
+		for($i=1;$i<=$this->n;$i++){
+			$sum = $sum->add($this->getBeta($i)->copy()->mul($this->getGamma($i)));
+		}
+		if(new RationalNumber(1, 2) == $sum){
+			$consistency = 2;
+		}
+
+		$sum1 = new RationalNumber(0);
+		$sum2 = new RationalNumber(0);
+		for($i=1;$i<=$this->n;$i++){
+			$sum1=$sum1->add($this->getBeta($i)->copy()->mul($this->getGamma($i))->mul($this->getGamma($i)));
+		}
+		for($i=1;$i<=$this->n;$i++){
+			for($j=1;$j<=$this->n;$j++){
+				$sum2=$sum2->add($this->getBeta($i)->copy()->mul($this->getGamma($i))->mul($this->getAlpha($i, $j)->copy()));
+			}
+		}
+		var_dump($sum2);
+		if(new RationalNumber(1, 3)== $sum1 && new RationalNumber(1, 6) == $sum2){
+			$consistency = 3;
+		}
+
+
+		return $consistency;
+	}
+
+	public function hasConsistency($n):boolean{
+		if($n === 1){
+			$sum=new RationalNumber(0);
+			for($i=1;$i<=$n;$i++){
+				$sum = $sum->add($this->getBeta($i));
+			}
+			return new RationalNumber(1, 0) === $sum;
+		}elseif($n === 2){
+
+		}elseif($n === 3){
+
+		}elseif($n === 4){
+
+		}
+	}
+}
---
+++ tests/Math/ButcherTableTest.php
@@ -0,0 +1,108 @@
+<?php 
+use PHPUnit\FrameWork\TestCase;
+
+use \Math\ButcherTable;
+use \Math\RationalNumber;
+class ButcherTableTest extends Testcase{
+    //https://de.wikipedia.org/wiki/Runge-Kutta-Verfahren#Beispiele
+    const expliciteEuler=[
+        [0, null],
+        [null, 1]
+    ];
+
+    const impliciteEuler=[
+        [1, 1],
+        [null, 1]
+    ];
+
+    const heun=[
+        [0, null, null],
+        [1, 1, null],
+        [0, "1/2", "1/2"]
+    ];
+
+    const rungeKutta2=[
+        [0, null, null],
+        ["1/2", "1/2", null],
+        [null, 0, 1]
+    ];
+
+    const impliciteTrapez2=[
+        [0, null, null],
+        [1, "1/2", "1/2"],
+        [null, "1/2", "1/2"]
+    ];
+
+    const rungeKutta3=[
+        [0, null, null, null],
+        ["1/2", "1/2", null, null],
+        [1, -1, 2, null],
+        [null, "1/6", "4/6", "1/6"]
+    ];
+
+    const heun3=[
+        [0, null, null, null],
+        [1/3, 1/3, null, null],
+        [2/3, 0, 2/3, null],
+        [null, 1/4, 0, 3/4]
+    ];
+
+    const rungeKutta4=[
+        [0, null, null, null, null],
+        [1/2, 1/2, null, null, null],
+        [1/2, null, 1/2, null, null],
+        [1, 0, 0, 1, null],
+        [null, 1/6, 1/3, 1/3, 1/6]
+    ];
+
+    public function testFromArrayAndGetter(){
+        $b = ButcherTable::fromArray(ButcherTableTest::expliciteEuler);
+        $this->assertEquals(new RationalNumber(0), $b->getGamma(1));
+        $this->assertEquals(new RationalNumber(0), $b->getAlpha(1, 1));
+        $this->assertEquals(new RationalNumber(1), $b->getBeta(1));
+
+        $b = ButcherTable::fromArray(ButcherTableTest::impliciteTrapez2);
+        $this->assertEquals(new RationalNumber(0), $b->getGamma(1));
+        $this->assertEquals(new RationalNumber(1), $b->getGamma(2));
+        $this->assertEquals(new RationalNumber(0), $b->getAlpha(1, 1));
+        $this->assertEquals(new RationalNumber(0), $b->getAlpha(1, 2));
+        $this->assertEquals(new RationalNumber(1, 2), $b->getAlpha(2, 1));
+        $this->assertEquals(new RationalNumber(1, 2), $b->getAlpha(2, 2));
+        $this->assertEquals(new RationalNumber(1, 2), $b->getBeta(1));
+        $this->assertEquals(new RationalNumber(1, 2), $b->getBeta(2));
+    }
+
+    /*public function testToArray(){
+
+    }*/
+
+    public function testConsistency(){
+        $b = ButcherTable::fromArray(ButcherTableTest::expliciteEuler);
+        $this->assertEquals(1, $b->getConistency());
+
+        $b = ButcherTable::fromArray(ButcherTableTest::impliciteEuler);
+        $this->assertEquals(1, $b->getConistency());
+
+        $b = ButcherTable::fromArray(ButcherTableTest::heun);
+        $this->assertEquals(2, $b->getConistency());
+
+        $b = ButcherTable::fromArray(ButcherTableTest::rungeKutta2);
+        $this->assertEquals(2, $b->getConistency());
+
+        $b = ButcherTable::fromArray(ButcherTableTest::impliciteTrapez2);
+        $this->assertEquals(2, $b->getConistency());
+
+        $b = ButcherTable::fromArray(ButcherTableTest::rungeKutta3);
+        $this->assertEquals(3, $b->getConistency());
+
+        $b = ButcherTable::fromArray(ButcherTableTest::heun3);
+        $this->assertEquals(3, $b->getConistency());
+
+        $b = ButcherTable::fromArray(ButcherTableTest::rungeKutta4);
+        $this->assertEquals(4, $b->getConistency());
+    }
+
+    public function testImplicite(){
+
+    }
+}
